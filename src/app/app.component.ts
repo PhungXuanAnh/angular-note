62b1f990df49279cf845125f3b4af9783608e6d0
Phung Xuan Anh <phungxuananh1991@gmail.com>
2019-09-17 11:43:39 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  // --------------------- Select tag sample with two-way binding -----------------
   levelNum:number;
   levels = [
       {num: 0, name: "AA"},
       {num: 1, name: "BB"}
   ];
-
   toNumber(){
     this.levelNum = +this.levelNum;
     console.log(this.levelNum);
   }
-
   selectedLevel = this.levels[0];
-
   selectedLevelCustomCompare = {num: 1, name: "BB"}
-
   compareFn(a, b) {
     console.log(a, b, a && b && a.num == b.num);
     return a && b && a.num == b.num;
   }
+  // -----------------------------------------------------------------------------
 }
